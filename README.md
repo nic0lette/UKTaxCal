b06b30ec367d564fdd87a93527029f64d0a5961b
Nikki Borrelli <borrelli@google.com>
2022-05-31 11:09:27 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # UKTaxCal
+
 Tool to help handle date calculations for new arrivals to the UK
+
+## iCal Events
+
+Events are matched by prefixes. For example, "GB Holiday: Spring Bank Holiday" and
+"GB Holiday: Boxing Day" are both recognized as bank holidays.
+
+Prefix | Meaning
+---|---
+GB Holiday: | Bank holiday
+PTO | Holiday (Paid Time Off)
+Sick | Sick day (Non-working)
+Working from | Day working outside the UK
+Travel | Days away from the UK
+
+"Travel" events are used to count the number of nights spent outside the UK.
+The script expects the start the day you leave (and counts it as the first
+"night" sleeping outside the UK), and ends the day _before_ returning,
+since you'd be sleeping in the UK that night.
+
+For example: Let's say I were to travel to Ireland for the weekend. I fly
+there on April 8th, and return on April 10. I'd be sleeping outside the UK
+the night between April 8th to April 9th, and April 9th to April 10th.
+I'd be flying back to the UK on the 10th, so I'd be sleeping here then.
+The travel event would be an all-day event from April 8th-April 9th.
+
+# Disclaimer
+
+While some amount of care was used to make this software, I am not a tax
+expert nor a lawyer in the UK (or anywhere else). Double check the numbers
+produced by the software before submitting them to HMRC (or don't blame
+me if they ended up being wrong.)
---
+++ main.py
@@ -0,0 +1,65 @@
+"""UK Tax Year Calendar Helper"""
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from icalevents.icalevents import events
+from icalevents.icalparser import Event
+import argparse
+
+
+def process_events(ical_path: str):
+    tax_year_start = datetime(2021, 4, 6)
+    tax_year_end = tax_year_start + relativedelta(years=1)
+    total_days_in_year = 365
+    total_working_days = 255
+
+    event_list = events(file=ical_path, fix_apple=True, start=tax_year_start, end=tax_year_end)
+    event_list.sort()
+    print(f"Found {len(event_list)} events")
+
+    bank_holidays = 0
+    sick_days = 0
+    special_holidays = 0
+    foreign_workdays = 0
+    nights_outside_uk = 0
+    personal_holiday = 0
+
+    for event in event_list:
+        if event is None:
+            event = Event()
+        days = (event.end - event.start).days
+        if event.summary.startswith("Travel"):
+            nights_outside_uk += days
+
+        if event.summary.startswith("GB Holiday:"):
+            bank_holidays += days
+        if event.summary.startswith("PTO"):
+            personal_holiday += days
+        if event.summary.startswith("Sick"):
+            sick_days += days
+        if event.summary.startswith("Google"):
+            special_holidays += days
+        if event.summary.startswith("Working from"):
+            foreign_workdays += days
+        print(f"Event: {event.summary}, {event.start} - {event.end} ({days} days)")
+
+    non_working_days = personal_holiday + sick_days + special_holidays
+
+    print(f"UK bank holidays: {bank_holidays}")
+    print(f"Sick days: {sick_days}")
+    print(f"Special Leave Days: {special_holidays}")
+    print(f"Foreign Workdays: {foreign_workdays}")
+    print(f"Nights sleeping outside the UK: {nights_outside_uk}")
+    print(f"----------------------------------------------------")
+    print(f"Nights in the UK: {total_days_in_year - nights_outside_uk}")
+    print(f"Days worked in UK: {total_working_days - non_working_days}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Process ical for UK Taxes")
+    parser.add_argument("--ical_file", dest="path", required=True)
+    args = parser.parse_args()
+    process_events(args.path)
+
+
+if __name__ == '__main__':
+    main()
